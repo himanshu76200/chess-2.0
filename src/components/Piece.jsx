6f5af8644dac8514bc19617c62a574f46cf45150
himanshu76200 <himanshu76200@gmail.com>
2021-05-28 11:30:11 +0530

    pieces added

--- src/components/Piece.jsx
+++ src/components/Piece.jsx
@@ -1,7 +1,13 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 
-const Piece = ({ piece }) => {
-  return <div>{piece.type}</div>;
+const Piece = ({ piece: { type, color } }) => {
+  const pieceImagePath = `assets/${type}_${color}.png`;
+  return (
+    <div className="piece-container">
+      <img src={pieceImagePath} className="piece" />
+    </div>
+  );
 };
 
 export default Piece;
