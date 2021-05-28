d1506d712c90e59fcb43f16321f0975d064dbcea
himanshu76200 <himanshu76200@gmail.com>
2021-05-28 11:18:01 +0530

    board completed

---
+++ src/components/Board.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import BoardSquare from "./BoardSquare";
+
+const Board = ({ board }) => {
+  const getXYPosition = (index) => {
+    const x = index % 8;
+    const y = Math.abs(Math.floor(index / 8) - 7);
+    return { x, y };
+  };
+  const isBlack = (index) => {
+    const { x, y } = getXYPosition(index);
+    return (x + y) % 2 === 1;
+  };
+
+  return (
+    <div className="board">
+      {board.flat().map((piece, index) => (
+        <div key={index} className="square">
+          <BoardSquare piece={piece} black={isBlack(index)} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Board;
---
+++ src/components/Piece.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Piece = ({ piece }) => {
+  return <div>{piece.type}</div>;
+};
+
+export default Piece;
