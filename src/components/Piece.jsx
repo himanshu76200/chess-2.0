e01e53330ef46d50b448efe9b81fc3f52e8fd681
himanshu76200 <himanshu76200@gmail.com>
2021-05-28 11:49:14 +0530

    dragging piece

--- src/components/Piece.jsx
+++ src/components/Piece.jsx
@@ -1,12 +1,27 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
+import { useDrag, DragPreviewImage } from "react-dnd";
 
 const Piece = ({ piece: { type, color } }) => {
+  const [{ isDragging }, drag, preview] = useDrag({
+    type: "piece",
+    id: `${type}_${color}`,
+    collect: (monitor) => {
+      return { isDragging: !!monitor.isDragging() };
+    },
+  });
   const pieceImagePath = `assets/${type}_${color}.png`;
   return (
-    <div className="piece-container">
-      <img src={pieceImagePath} className="piece" />
-    </div>
+    <>
+      <DragPreviewImage connect={preview} src={pieceImagePath} />
+      <div
+        className="piece-container"
+        ref={drag}
+        style={{ opacity: isDragging ? 0 : 1 }}
+      >
+        <img src={pieceImagePath} className="piece" />
+      </div>
+    </>
   );
 };
 
